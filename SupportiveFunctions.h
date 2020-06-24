8dda6bc63bfa484f9677b11e66d1a71a84285f8b
Kilabarus <63799260+Kilabarus@users.noreply.github.com>
2020-06-24 15:21:27 +0300

    Delete SupportiveFunctions.h

--- SupportiveFunctions.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "DateAndTime.h"
-#pragma once
-
-unsigned InputUnsigned(std::string msg, unsigned min = 0, unsigned max = UINT_MAX);
-
-std::string InputString(std::string msg);
-
-std::string CharArrayToString(char* a);
